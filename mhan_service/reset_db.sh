32318d78fa6c7ac92ba788f599526be0544067a7
Nathalie D'Amours <nathalie.damours@gmail.com>
2016-08-02 13:13:39 -0400

    Enforce python version 3

--- mhan_service/reset_db.sh
+++ mhan_service/reset_db.sh
@@ -2,11 +2,11 @@
 mysql -u root -p$SQL_PASSWORD -e "drop database followme"
 mysql -u root -p$SQL_PASSWORD -e  "create database followme"
 rm followme/migrations/*_initial.py
-python manage.py migrate
-python manage.py makemigrations followme
-python manage.py sqlmigrate followme 0001
+python3 manage.py migrate
+python3 manage.py makemigrations followme
+python3 manage.py sqlmigrate followme 0001
 bold=$(tput bold)
 normal=$(tput sgr0)
-python manage.py migrate
+python3 manage.py migrate
 echo "${bold}When prompted enter email and password for the admin user${normal}"
-python manage.py createsuperuser --username admin
+python3 manage.py createsuperuser --username admin
